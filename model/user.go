b0f0cc774e8d77cd1a4663551ead16de4579328c
Vladimir Jigulin <mogaika@yandex.ru>
2017-01-23 14:45:29 +0400

    User model implemented

--- model/user.go
+++ model/user.go
@@ -1,13 +1,64 @@
 package model
 
 import (
+	"strings"
 	"time"
+
+	"github.com/molecul/qa_portal/util/database"
+	"github.com/zalando/gin-oauth2/google"
 )
 
 type User struct {
-	ID      int64 `xorm:"pk autoincr"`
-	Score   int64
-	Created time.Time `xorm:"created"`
-	Updated time.Time `xorm:"updated"`
-	// TODO OAuth sutff
+	ID            int64 `xorm:"pk autoincr"`
+	Score         int64
+	Created       time.Time `xorm:"created"`
+	Updated       time.Time `xorm:"updated"`
+	Email         string
+	EmailVerified bool
+	Picture       string
+	Name          string
+}
+
+func GetUserById(id int64) (*User, error) {
+	u := new(User)
+	has, err := database.Get().Id(id).Get(u)
+	if err != nil {
+		return nil, err
+	}
+	if !has {
+		return nil, nil
+	}
+	return u, nil
+}
+
+func GetUserByEmail(email string) (*User, error) {
+	email = strings.ToLower(email)
+	u := &User{Email: email}
+	has, err := database.Get().Get(u)
+	if err != nil {
+		return nil, err
+	}
+	if !has {
+		return nil, nil
+	}
+	return u, nil
+}
+
+func CreateUser(u *User) error {
+	u.Email = strings.ToLower(u.Email)
+	_, err := database.Get().InsertOne(u)
+	return err
+}
+
+func (u *User) Update() error {
+	_, err := database.Get().Id(u.ID).AllCols().Update(u)
+	return err
+}
+
+func (u *User) FillFromGoogle(gu *google.User) *User {
+	u.Email = strings.ToLower(gu.Email)
+	u.EmailVerified = gu.EmailVerified
+	u.Name = gu.Name
+	u.Picture = gu.Picture
+	return u
 }
--- web/handlers/web_handlers.go
+++ web/handlers/web_handlers.go
@@ -2,12 +2,44 @@ package webHandlers
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/molecul/qa_portal/model"
 	"github.com/zalando/gin-oauth2/google"
 )
 
+func doError(ctx *gin.Context, err error) {
+	ctx.JSON(http.StatusInternalServerError, gin.H{"Internal error": err.Error()})
+	panic(err)
+}
+
 func UserInfoHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"Hello": "from private", "user": ctx.MustGet("user").(google.User)})
+	gu := ctx.MustGet("user").(google.User)
+	usr, err := model.GetUserByEmail(gu.Email)
+	if err != nil {
+		doError(ctx, err)
+		return
+	}
+	if usr == nil {
+		if err = model.CreateUser(new(model.User).FillFromGoogle(&gu)); err != nil {
+			doError(ctx, err)
+			return
+		}
+		// TODO ONLY FOR DEBUG. NEED REMOVE
+		usr, err = model.GetUserByEmail(gu.Email)
+		if err != nil {
+			doError(ctx, err)
+			return
+		}
+		// ================================
+	} else {
+		if err = usr.FillFromGoogle(&gu).Update(); err != nil {
+			doError(ctx, err)
+			return
+		}
+	}
+	ctx.JSON(http.StatusOK, gin.H{"Hello": "from private", "user": gu, "internal_user": usr})
+
 }
 
 func MainPageHandler(c *gin.Context) {
