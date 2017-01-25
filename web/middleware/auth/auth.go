db651cf73268fb4c78481d1099017740658b50fd
Vladimir Jigulin <mogaika@yandex.ru>
2017-01-25 21:54:43 +0400

    Refactor auth middleware. Add cleanups

--- web/middleware/auth/auth.go
+++ web/middleware/auth/auth.go
@@ -79,7 +79,7 @@ func Logout(ctx *gin.Context, redirect string) {
 	views.RenderRedirect(ctx, redirect)
 }
 
-func userDataUpdate(ctx *gin.Context, gu *User) {
+func userDataUpdate(ctx *gin.Context, gu *User, session sessions.Session) {
 	usr, err := model.GetUserByEmail(gu.Email)
 	if err != nil {
 		views.RenderError(ctx, err)
@@ -96,56 +96,66 @@ func userDataUpdate(ctx *gin.Context, gu *User) {
 			return
 		}
 	}
-	session := sessions.Default(ctx)
 	session.Set(userIdKey, usr.ID)
-	session.Save()
+}
+
+func googleUserFromAuthHandler(ctx *gin.Context, session sessions.Session) *User {
+	retrievedState := session.Get("state")
+	if retrievedState != ctx.Query("state") {
+		ctx.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+		return nil
+	}
+
+	tok, err := conf.Exchange(oauth2.NoContext, ctx.Query("code"))
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return nil
+	}
+
+	client := conf.Client(oauth2.NoContext, tok)
+	email, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return nil
+	}
+	defer email.Body.Close()
+	data, err := ioutil.ReadAll(email.Body)
+	if err != nil {
+		glog.Errorf("[Gin-OAuth] Could not read Body: %s", err)
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return nil
+	}
+
+	user := new(User)
+	err = json.Unmarshal(data, user)
+	if err != nil {
+		glog.Errorf("[Gin-OAuth] Unmarshal userinfo failed: %s", err)
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return nil
+	}
+	return user
 }
 
 func AuthRedirectHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// Handle the exchange code to initiate a transport.
 		session := sessions.Default(ctx)
-		retrievedState := session.Get("state")
-		if retrievedState != ctx.Query("state") {
-			ctx.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
-			return
+
+		gu := googleUserFromAuthHandler(ctx, session)
+
+		if gu != nil {
+			userDataUpdate(ctx, gu, session)
 		}
 
-		tok, err := conf.Exchange(oauth2.NoContext, ctx.Query("code"))
-		if err != nil {
-			ctx.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-
-		client := conf.Client(oauth2.NoContext, tok)
-		email, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
-		if err != nil {
-			ctx.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-		defer email.Body.Close()
-		data, err := ioutil.ReadAll(email.Body)
-		if err != nil {
-			glog.Errorf("[Gin-OAuth] Could not read Body: %s", err)
-			ctx.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		user := new(User)
-		err = json.Unmarshal(data, user)
-		if err != nil {
-			glog.Errorf("[Gin-OAuth] Unmarshal userinfo failed: %s", err)
-			ctx.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		// save userinfo
-		userDataUpdate(ctx, user)
+		redirectTarget := "/"
 		if redirect := session.Get("redirect"); redirect != nil {
-			views.RenderRedirect(ctx, redirect.(string))
-		} else {
-			views.RenderRedirect(ctx, "/")
+			redirectTarget = redirect.(string)
 		}
+
+		session.Delete("state")
+		session.Delete("redirect")
+		session.Save()
+
+		views.RenderRedirect(ctx, redirectTarget)
 	}
 }
 
