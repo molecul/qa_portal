8417e98e9241d689b9e0be728f074f4e788fd921
Alexey Galkin <agalkin@mirantis.com>
2017-01-27 18:04:32 +0400

    Add challenge page in webview
    Change-Id: Ib0ef2f1adcbd02d04bc93398f6475eb2fa3a20e0

--- web/handlers/handlers.go
+++ web/handlers/handlers.go
@@ -19,6 +19,11 @@ func MainPageHandler(ctx *gin.Context) {
 		"user": current_user})
 }
 
+func ChallengesWebHandler(ctx *gin.Context) {
+	current_user := auth.GetUser(ctx)
+	ctx.HTML(http.StatusOK, "pages/challenges", gin.H{"user": current_user})
+}
+
 func LoginHandler(ctx *gin.Context) {
 	auth.Login(ctx, "/")
 }
