95f338f9c454a44b52ef80e9d6ce4607d4797d0b
Alexey Galkin <agalkin@mirantis.com>
2017-01-30 16:24:33 +0400

    Implement dataFormatter for history page
    Change-Id: Id58efc8d1ebaac50ec88874afb7f588afa70c561

--- web/static/js/qa_portal.js
+++ web/static/js/qa_portal.js
@@ -12,4 +12,41 @@ var qa_portal = {
 			},
 		});
 	},
+	initProfile: function() {
+		$('table#history').bootstrapTable({
+		    url: '/api/history',
+			showRefresh: true,
+		    columns: [
+				{ field: 'Id', title: 'Id' },
+				{ field: 'Challenge',title: 'Challenge' },
+				{ field: 'State', title: 'State',
+					formatter: function(data, row, index) {
+					    switch (data) {
+					        case 1:
+					            return '<span class="label label-success">Success</span>';
+					            break
+					        case 2:
+					            return '<span class="label label-danger">Failed</span>';
+					            break
+							case 0:
+								return '<span class="label label-info">In progress</span>';
+								break
+					        default:
+					            return '<span class="label label-warning">Undefined</span>';
+					    }
+					},
+				},
+				{ field: 'Duration',title: 'Duration' },
+			],
+			onClickRow: function(row, $element, field) {
+				switch(field) {
+					case "Challenge":
+						window.location.href = '/solve/' + row.ChallengeId;
+						break
+					default:
+						window.location.href = '/test/' + row.Id;
+				}
+			},
+		});
+	},
 }
